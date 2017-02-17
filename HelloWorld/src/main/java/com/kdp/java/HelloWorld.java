ba70a65889bac94da6e5076088a003b1eb18e19c
Kaz Al-Kazwini <malkazwini@users.noreply.github.com>
2017-02-16 21:09:38 -0800

    Update HelloWorld.java

--- HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
+++ HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
@@ -10,11 +10,7 @@ public class HelloWorld {
 		System.out.println("Hello World!!!");
 		System.out.println("Hi");
 		 int age = 0;
-      			age = age + 7;
-   		   System.out.println("Puppy age is : " + age);
+      		 age = age + 7;
+   		 System.out.println("Puppy age is : " + age);
 	}
-  public static void main(String args[]) {
-      Test test = new Test();
-      test.pupAge();
-   }
 }
