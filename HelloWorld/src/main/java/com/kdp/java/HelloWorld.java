1589465c02c53c92844635b3caa1e5a6c8c89d27
Kaz Al-Kazwini <malkazwini@users.noreply.github.com>
2017-02-16 21:00:33 -0800

    Update HelloWorld.java

--- HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
+++ HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
@@ -8,6 +8,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
+		System.out.println("Hi");
+		 int age = 0;
+      			age = age + 7;
+   		   System.out.println("Puppy age is : " + age);
 	}
-
+  public static void main(String args[]) {
+      Test test = new Test();
+      test.pupAge();
+   }
 }
